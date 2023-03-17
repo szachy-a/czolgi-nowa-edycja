efd29dd2e6288c6969623abfe8a450d22d7c611a
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-17 16:49:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ realScreen = pygame.display.set_mode((1366, 768))
 screen = pygame.Surface((1920, 1080))
 
 running = True
+wszystko = pygame.sprite.Group()
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
         screen.fill((255, 255, 255))
+        for duszek in wszystko:
+            screen.blit(duszek.surf, duszek.rect)
         pygame.transform.scale(screen, realScreen.get_size(), realScreen)
         pygame.display.flip()
 pygame.quit()
