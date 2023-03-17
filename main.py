21d41e8f0a08ba4cf8bd5a589becfda415d10446
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-17 16:42:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from pygame.locals import (
     QUIT
 )
 pygame.init()
+
+realScreen = pygame.display.set_mode((1366, 768))
+screen = pygame.Surface((1920, 1080))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        screen.fill((255, 255, 255))
+        pygame.transform.scale(screen, realScreen.get_size(), realScreen)
+        pygame.display.flip()
