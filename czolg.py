2a6d7ca97c5d919deeff70bc9a3500630c20bb08
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-17 17:02:07 +0100

    q

--- czolg.py
+++ czolg.py
@@ -2,6 +2,8 @@ import pygame
 
 import abc
 
+CZOLG_W_LEWO, CZOLG_W_PRAWO, CZOLG_W_PRZOD, CZOLG_W_TYL, LUFA_W_LEWO, LUFA_W_PRAWO, STRZAL, *_ = range(100)
+
 class Czolg(abc.ABC, pygame.sprite.Sprite):
     @abc.abstractmethod
     def __init__(self, x, y):
--- czolgGracza.py
+++ czolgGracza.py
@@ -1,4 +1,10 @@
 import pygame
+from pygame.locals import (
+    K_a,
+    K_d,
+    K_w,
+    K_s
+)
 
 import czolg
 
@@ -7,3 +13,8 @@ class CzolgGracza(czolg.Czolg):
         super().__init__(x, y)
         self.surf = pygame.image.load('czolgGracza.png').convert()
         self.rect = self.surf.get_rect(topleft=(x, y))
+    def podejmijDecyzje(self):
+        klawisze = pygame.key.get_pressed()
+        if klawisze[K_a]:
+            return
+        if klawisze[K_w]:
