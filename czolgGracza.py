c4825e8f3daa333f8f08d4dc770dc1e5ba200c16
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-17 16:53:26 +0100

    Update czolgGracza.py

--- czolgGracza.py
+++ czolgGracza.py
@@ -5,4 +5,5 @@ import czolg
 class CzolgGracza(czolg.Czolg):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.surf = pygame.image.load()
+        self.surf = pygame.image.load('czolgGracza.png').convert()
+        self.rect = self.surf.get_rect(topleft=(x, y))
