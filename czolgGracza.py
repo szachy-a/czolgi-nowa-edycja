31c53e4d56946e130952199665dabc6f9db07b64
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-17 17:05:55 +0100

    Update czolgGracza.py

--- czolgGracza.py
+++ czolgGracza.py
@@ -11,10 +11,16 @@ import czolg
 class CzolgGracza(czolg.Czolg):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.surf = pygame.image.load('czolgGracza.png').convert()
+        self.surf = pygame.image.load('czolgGracza.jpg').convert()
         self.rect = self.surf.get_rect(topleft=(x, y))
     def podejmijDecyzje(self):
         klawisze = pygame.key.get_pressed()
         if klawisze[K_a]:
-            return
+            return czolg.CZOLG_W_LEWO
+        if klawisze[K_d]:
+            return czolg.CZOLG_W_PRAWO
         if klawisze[K_w]:
+            return czolg.CZOLG_W_PRZOD
+        if klawisze[K_s]:
+            return czolg.CZOLG_W_TYL
+        
