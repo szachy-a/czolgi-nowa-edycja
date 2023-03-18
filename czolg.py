77eea97da2a4650355c6805207c4235488c61fed
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-18 13:14:44 +0100

    q

--- czolg.py
+++ czolg.py
@@ -2,8 +2,18 @@ import pygame
 
 import abc
 import czolg
+import math
 
 CZOLG_W_LEWO, CZOLG_W_PRAWO, CZOLG_W_PRZOD, CZOLG_W_TYL, STRZAL, BRAK_AKCJI, *_ = range(100)
+
+WEKTORY = [pygame.math.Vector2() for _ in range(360)]
+for i, v in enumerate(WEKTORY):
+    v.from_polar((1, i))
+    try:
+        v.normalize_ip()
+    except ValueError:
+        pass
+    v.x = -v.x
 
 class Czolg(abc.ABC, pygame.sprite.Sprite):
     @abc.abstractmethod
@@ -17,10 +27,12 @@ class Czolg(abc.ABC, pygame.sprite.Sprite):
                 self.kat += 3
                 self.surf = pygame.transform.rotate(self.orgSurf, self.kat)
                 self.rect = self.surf.get_rect(center=self.rect.center)
+                self.wektor = WEKTORY[self.kat]
             case czolg.CZOLG_W_PRAWO:
                 self.kat -= 3
                 self.surf = pygame.transform.rotate(self.orgSurf, self.kat)
                 self.rect = self.surf.get_rect(center=self.rect.center)
+                self.wektor = WEKTORY[self.kat]
             case czolg.CZOLG_W_PRZOD:
                 self.rect.x += self.wektor.x * 3
                 self.rect.y += self.wektor.y * 3
