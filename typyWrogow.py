84352192878587dda89d2263cf87f8d7d70cb3d0
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-19 19:22:56 +0100

    q

--- typyWrogow.py
+++ typyWrogow.py
@@ -5,7 +5,7 @@ import czolgWroga
 class Solider(czolgWroga.CzolgWroga):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.orgSurf = pygame.image.load('solider.png').convert_alpha()
+        self.orgSurf = pygame.image.load('solider.png').convert()
         self.surf = self.orgSurf.copy()
         self.rect = self.surf.get_rect(topleft=(x, y))
         self.mask = pygame.mask.from_surface(self.surf)
@@ -19,7 +19,8 @@ class Solider(czolgWroga.CzolgWroga):
 class Scout(czolgWroga.CzolgWroga):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.orgSurf = pygame.image.load('solider.png').convert_alpha()
+        self.orgSurf = pygame.image.load('scout.png').convert_alpha()
+        self.orgSurf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
         self.surf = self.orgSurf.copy()
         self.rect = self.surf.get_rect(topleft=(x, y))
         self.mask = pygame.mask.from_surface(self.surf)
@@ -28,4 +29,19 @@ class Scout(czolgWroga.CzolgWroga):
         self.zadajeObrazen = 1
         self.hp = self.maxWytrzymalosc
         self.maxCooldown = 6 * 30
+        self.cooldown = 0
+
+class Juggernaut(czolgWroga.CzolgWroga):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.orgSurf = pygame.image.load('juggernaut.png').convert()
+        self.orgSurf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+        self.surf = self.orgSurf.copy()
+        self.rect = self.surf.get_rect(topleft=(x, y))
+        self.mask = pygame.mask.from_surface(self.surf)
+        self.maxWytrzymalosc = 4
+        self.odlegloscStrzalu = 2.5
+        self.zadajeObrazen = 1
+        self.hp = self.maxWytrzymalosc
+        self.maxCooldown = 10 * 30
         self.cooldown = 0
