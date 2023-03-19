4df531124a4879ea5732673ec6598bb36c9dbd90
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-03-19 14:05:56 +0100

    Update czolgWroga.py

--- czolgWroga.py
+++ czolgWroga.py
@@ -4,6 +4,7 @@ import abc
 import algorytmy
 import math
 import czolgGracza as cg
+import pocisk
 
 import __main__
 
@@ -15,39 +16,11 @@ class CzolgWroga(czolg.Czolg):
         self.zadajeObrazen = 1
         self.hp = self.maxWytrzymalosc
     def podejmijDecyzje(self):
-        graf = __main__.graf
-        wszystko = __main__.wszystko
-        szerokoscEkranu = __main__.screen.get_width()
-        wysokoscEkranu = __main__.screen.get_height()
-        for x in wszystko:
-            if type(x) == cg.CzolgGracza:
-                czolgGracza = x
-                break
-        sciezka = algorytmy.znajdzSciezke(self.rect.centerx, self.rect.centery, czolgGracza.rect.centerx, czolgGracza.rect.centery, graf, szerokoscEkranu, wysokoscEkranu)
-        if sciezka == "sciana":
-            for x in graf.keys():
-                if algorytmy.obliczDystans(self.rect.centerx, self.rect.centery, x[0], x[1]) <= math.sqrt((szerokoscEkranu//40)**2 + (wysokoscEkranu//40)**2):
-                    sciezka = [0, (x[0], x[1])]
-                    break
-        elif sciezka == "graczNiedostepny":
-            for x in graf.keys():
-                if algorytmy.obliczDystans(czolgGracza.rect.centerx, czolgGracza.rect.centery, x[0], x[1]) <= math.sqrt((szerokoscEkranu//40)**2 + (wysokoscEkranu//40)**2):
-                    sciezka = algorytmy.znajdzSciezke(self.rect.centerx, self.rect.centery, x[0], x[1], graf, szerokoscEkranu, wysokoscEkranu)
-                    break
-        if algorytmy.obliczDystans(self.rect.centerx, self.rect.centery, czolgGracza.rect.centerx, czolgGracza.rect.centery) >= self.odlegloscStrzalu * 10:
-            kat = algorytmy.obliczKat(self.rect.centerx,self.rect.centery,sciezka[1][0], sciezka[1][1])
-            if self.kat <= kat-3:
-                return czolg.CZOLG_W_LEWO
-            elif self.kat >= kat+3:
-                return czolg.CZOLG_W_PRAWO
-            else:
-                if algorytmy.obliczDystans(self.rect.centerx, self.rect.centery, sciezka[1][0], sciezka[1][1]) >= 3:
-                    return czolg.CZOLG_W_PRZOD
-        else:
-            kat = algorytmy.obliczKat(self.rect.centerx,self.rect.centery,sciezka[0][0], sciezka[0][1])
-            if self.kat <= kat-3:
-                return czolg.CZOLG_W_LEWO
-            elif self.kat >= kat+3:
-                return czolg.CZOLG_W_PRAWO
-            else:
+        p = pocisk.Pocisk(*self.rect.center, self.wektor, 1000, 0, self)
+        g = pygame.sprite.Group(p)
+        for i in range(900):
+            p.update(pygame.sprite.Group(__main__.g))
+            if p not in g:
                 return czolg.STRZAL
+        else:
+            return czolg.CZOLG_W_LEWO
